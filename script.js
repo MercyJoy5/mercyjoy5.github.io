a4730b053a53db18ced65dee0900145f7beaabe0
MercyJoy5 <joywamalwa6@gmail.com>
2026-02-16 10:25:47 +0300

    Update script.js

--- script.js
+++ script.js
@@ -24,10 +24,12 @@ document.addEventListener("DOMContentLoaded", function () {
 
         // Clear the form
         form.reset();
-        const toggle = document.getElementById("themeToggle");
+       const toggle = document.getElementById("themeToggle");
 
 toggle.addEventListener("click", function () {
-    document.body.classList.toggle("dark");
+    document.body.classList.toggle("light");
+});
+
 });
 
     });
